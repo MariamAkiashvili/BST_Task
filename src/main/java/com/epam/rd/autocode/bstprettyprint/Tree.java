41047d313bac39886c32533d9493340fb56f256e
Mariam Akiashvili <mari.akiashvili@gmail.com>
2024-06-02 23:56:40 +0400

    fixed version

--- src/main/java/com/epam/rd/autocode/bstprettyprint/Tree.java
+++ src/main/java/com/epam/rd/autocode/bstprettyprint/Tree.java
@@ -19,10 +19,8 @@ public class Tree implements PrintableTree {
     @Override
     public String prettyPrint() {
 
-        traverse(root, 0, true, true, 0);
-//        printed.setLength(0); // Clear the StringBuilder
-//        traverse(root, 0, true, true, new HashSet<>());
-        return printed.toString();
+        traverse(root, new StringBuilder("\n") ,0, true, true);
+        return printed.append("\n").substring(1);
     }
 
     private Node addRecursive(Node current, int value) {
@@ -32,42 +30,59 @@ public class Tree implements PrintableTree {
         }
         if (current.data > value) {
             current.leftNode = addRecursive(current.leftNode, value);
-        } else {
+        } else if (current.data < value) {
             current.rightNode = addRecursive(current.rightNode, value);
         }
         return current;
     }
 //    private void traverse(Node root, int space, boolean isLeft, boolean isRoot, Set<Integer> verticalLines) {
 
-    private void traverse(Node root, int space, boolean isLeft, boolean isRoot, int stick ) {
+    private void traverse(Node root, StringBuilder lineSB, int space, boolean isLeft, boolean isRoot) {
         //BaseCase
         if (root == null) {
             return;
         }
 
-        space += String.valueOf(root.data).length();
+        int dataSize = Integer.toString(root.data).length();
+        int depth = lineSB.length();
+        int i = "\n │".indexOf(lineSB.charAt(depth-1));
+        int j = (root.leftNode != null ? 2 : 0) + (root.rightNode != null ? 1:0);
 
+        lineSB.append(" ".repeat((dataSize +1)));
+
+
+        space += String.valueOf(root.data).length();
         space += isRoot ? -1 : 1;
 
-        traverse(root.leftNode, space, true, false, stick);
+        traverse(root.leftNode, lineSB, space, true, false);
+        lineSB.setLength(depth -1);
 
+//        printed.append(lineSB);
+//
+//        printed.append(!isRoot && isLeft ? "┌" : "");
+//        printed.append(!isRoot && !isLeft ? "└" : "");
+//        printed.append(root.data);
+//        if (root.rightNode != null && root.leftNode != null) {
+//            printed.append("┤");
+//        } else if (root.leftNode != null) {
+//            printed.append("┘");
+//        } else if (root.rightNode != null) {
+//            printed.append("┐");
+//        }
 
-        printed.append(" ".repeat(Math.max(0, space - String.valueOf(root.data).length())));
-        printed.append(!isRoot && isLeft ? "┌" : "");
-        printed.append(!isRoot && !isLeft ? "└" : "");
+        printed.append(lineSB);
+        printed.append("\n┌└".charAt(i));
         printed.append(root.data);
-        if (root.rightNode != null && root.leftNode != null) {
-            printed.append("┤");
-        } else if (root.leftNode != null) {
-            printed.append("┘");
-        } else if (root.rightNode != null) {
-            printed.append("┐");
-        }
-        printed.append("\n");
+        printed.append(" ┐┘┤".charAt(j));
+        printed.setLength(root.rightNode==null && root.leftNode==null ? printed.length()-1: printed.length());
 
+        lineSB.append("\n│ ".charAt(i));
+        lineSB.append(" ".repeat(dataSize));
+        lineSB.append(" │ │".charAt(j));
 
-        traverse(root.rightNode, space, false, false, stick);
+        traverse(root.rightNode, lineSB, space, false, false);
 
     }
 
 }
+
